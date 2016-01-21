816d1b4380fce523e06823007d4891bbe1dd210b
Blixa Morgan <blixa@projectmakeit.com>
2016-01-21 12:20:27 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+express = require("express")
+fs = require("fs")
+
+app = express()
+wordsFile = fs.readFileSync("dd-words.txt")
+console.log(wordsFile)
+words = wordsFile.toString().split(/\s+/)
+console.log(words)
+words.pop()
+
+app.get("/", function(req,res){
+	word1= Math.floor(words.length*Math.random())
+	word2= Math.floor(words.length*Math.random())
+	word1 = words[word1]
+	word2 = words[word2]
+	console.log("sent pair "+word1+" "+word2)
+	res.send(word1+" "+word2)
+})
+
+app.listen(3030)
+
+
+
